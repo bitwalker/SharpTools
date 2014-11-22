a9814ca2f3a222f040710461132b648250425e9a
Paul Schoenfelder <paulschoenfelder@gmail.com>
2014-11-22 02:23:25 -0600

    Remove redundant locking from AutoIncrementingIdentifierGenerators

--- SharpTools/Testing/EntityFramework/Internal/Id/AutoIncrementingIdentifierGenerators.cs
+++ SharpTools/Testing/EntityFramework/Internal/Id/AutoIncrementingIdentifierGenerators.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Runtime.CompilerServices;
 
 namespace SharpTools.Testing.EntityFramework.Internal.Id
@@ -7,60 +6,48 @@ namespace SharpTools.Testing.EntityFramework.Internal.Id
     [DebuggerDisplay("{_counter}", Name = "AutoIncrementingShortIdentifierGenerator")]
     internal sealed class AutoIncrementingShortIdentifierGenerator : IIdentifierGenerator
     {
-        private readonly object _lock = new object();
         private short _counter;
 
         [MethodImpl(MethodImplOptions.Synchronized)]
         public object Generate()
         {
-            lock (_lock)
-            {
-                if (_counter < 0)
-                    _counter = 0;
+            if (_counter < 0)
+                _counter = 0;
 
-                _counter++;
-                return _counter;
-            }
+            _counter++;
+            return _counter;
         }
     }
 
     [DebuggerDisplay("{_counter}", Name = "AutoIncrementingIntegerIdentifierGenerator")]
     internal sealed class AutoIncrementingIntegerIdentifierGenerator : IIdentifierGenerator
     {
-        private readonly object _lock = new object();
         private int _counter;
 
         [MethodImpl(MethodImplOptions.Synchronized)]
         public object Generate()
         {
-            lock (_lock)
-            {
-                if (_counter < 0)
-                    _counter = 0;
+            if (_counter < 0)
+                _counter = 0;
 
-                _counter++;
-                return _counter;
-            }
+            _counter++;
+            return _counter;
         }
     }
 
     [DebuggerDisplay("{_counter}", Name = "AutoIncrementingLongIdentifierGenerator")]
     internal sealed class AutoIncrementingLongIdentifierGenerator : IIdentifierGenerator
     {
-        private readonly object _lock = new object();
         private long _counter;
 
         [MethodImpl(MethodImplOptions.Synchronized)]
         public object Generate()
         {
-            lock (_lock)
-            {
-                if (_counter < 0)
-                    _counter = 0;
+            if (_counter < 0)
+                _counter = 0;
 
-                _counter++;
-                return _counter;
-            }
+            _counter++;
+            return _counter;
         }
     }
 }
