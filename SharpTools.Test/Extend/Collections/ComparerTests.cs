fde8e585c68edabd11c788993a009cbbc1b8bfd3
Paul Schoenfelder <paulschoenfelder@gmail.com>
2014-11-22 05:15:46 -0600

    Add MultiComparer, and tests for Multi and GenericComparers

---
+++ SharpTools.Test/Extend/Collections/ComparerTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpTools.Extend.Collections;
+using SharpTools.Reflection;
+
+namespace SharpTools.Test.Extend.Collections
+{
+    [TestClass]
+    public class ComparerTests
+    {
+        [TestMethod]
+        public void GenericComparer_Compare()
+        {
+            var descendingLastNames = GenericComparer<Person>.Create(p => p.LastName, false);
+            var ascendingFirstNames = GenericComparer<Person>.Create(p => p.FirstName, true);
+
+            var people = new List<Person>();
+            people.Add(new Person("Alex", "Peters"));
+            people.Add(new Person("Peter", "Smith"));
+            people.Add(new Person("Paul", "Schoenfelder"));
+            people.Add(new Person("Alex", "Smith"));
+
+            people.Sort(descendingLastNames);
+
+            var expected = new[] {"Smith", "Smith", "Schoenfelder", "Peters"};
+            var result = people.Select(p => p.LastName).ToArray();
+            Assert.IsTrue(expected.SequenceEqual(result));
+
+            people.Sort(ascendingFirstNames);
+            expected = new[] {"Alex", "Alex", "Paul", "Peter"};
+            result = people.Select(p => p.FirstName).ToArray();
+            Assert.IsTrue(expected.SequenceEqual(result));
+        }
+
+        [TestMethod]
+        public void MultiComparer_Compare()
+        {
+            // Sort first by ascending first names, then by descending last name
+            var ascFirstNames = new GenericComparer<Person, string>(p => p.FirstName, true);
+            var descLastNames = new GenericComparer<Person, string>(p => p.LastName, false);
+            var comparer = new MultiComparer<Person>(ascFirstNames, descLastNames);
+
+            var people = new List<Person>();
+            people.Add(new Person("Alex", "Peters"));
+            people.Add(new Person("Peter", "Smith"));
+            people.Add(new Person("Paul", "Schoenfelder"));
+            people.Add(new Person("Alex", "Smith"));
+
+            people.Sort(comparer);
+
+            var expected = new[] {"Alex Smith", "Alex Peters", "Paul Schoenfelder", "Peter Smith"};
+            var result = people.Select(p => string.Format("{0} {1}", p.FirstName, p.LastName)).ToArray();
+            Assert.IsTrue(expected.SequenceEqual(result));
+        }
+
+        [TestMethod]
+        public void MultiComparer_Helpers()
+        {
+            // Sort (asc order) by first name, then by last name
+            var comparer = MultiComparer<Person>.Create(p => p.FirstName, p => p.LastName);
+
+            var people = new List<Person>();
+            people.Add(new Person("Alex", "Peters"));
+            people.Add(new Person("Peter", "Smith"));
+            people.Add(new Person("Paul", "Schoenfelder"));
+            people.Add(new Person("Alex", "Smith"));
+
+            people.Sort(comparer);
+
+            var expected = new[] {"Alex Peters", "Alex Smith", "Paul Schoenfelder", "Peter Smith"};
+            var result = people.Select(p => string.Format("{0} {1}", p.FirstName, p.LastName)).ToArray();
+            Assert.IsTrue(expected.SequenceEqual(result));
+        }
+
+        public class Person
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+            public Person(string first, string last)
+            {
+                FirstName = first;
+                LastName = last;
+            }
+        }
+    }
+}
---
+++ SharpTools/Extend/Collections/MultiComparer.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SharpTools.Extend.Collections
+{
+    /// <summary>
+    /// Merges two or more comparers into a single comparer, applying them in order.
+    /// Comparisons are performed in the order they were provided, but subsequent
+    /// comparisons are only performed if the previous one returned 0, i.e. equivalent.
+    ///
+    /// The purpose of this class is to allow one to perform complex comparisons beyond
+    /// a simple ascending or descending ordering. A few examples of such comparisons are:
+    /// 
+    /// - sort a list of Orders by ascending OrderDate, but if two Orders were placed on
+    ///   the same day, sort them by OrderStatus in order of priority
+    /// - sort a list of People by FirstName, then by LastName, then by DateOfBirth in
+    ///   reverse chronological order (youngest people appear first).
+    ///
+    /// As you can see, combined comparisons like those above are exceedingly common, but
+    /// unless you want to hand write multiple <see cref="IComparer{T}"/> implementations,
+    /// one for each combination of conditions, you are going to want something better. That's
+    /// what <see cref="MultiComparer{T}"/> aims to provide!
+    /// </summary>
+    /// <typeparam name="T">The type to compare</typeparam>
+    public sealed class MultiComparer<T> : IComparer<T>, IComparer
+    {
+        private readonly IComparer<T>[] _comparers; 
+
+        /// <summary>
+        /// Creates a <see cref="MultiComparer{T}"/> given a collection of <see cref="IComparer{T}"/>.
+        /// </summary>
+        /// <param name="comparers">The comparers to use, in order of priority</param>
+        public MultiComparer(params IComparer<T>[] comparers)
+        {
+            if (comparers == null || comparers.Length < 2)
+                throw new ArgumentException("You must provide at least two comparers!");
+
+            _comparers = comparers;
+        }
+
+        /// <summary>
+        /// Create a <see cref="MultiComparer{T}"/> from two lambda expressions.
+        /// Optionally provide a flag to sort in ascending/descending order (default is ascending).
+        /// This is a shortcut for creating two <see cref="GenericComparer{T}"/> and providing them
+        /// as constructor parameters to <see cref="MultiComparer{T}"/>
+        /// </summary>
+        /// <typeparam name="U">The result type of the primary expression</typeparam>
+        /// <typeparam name="V">The result type of the secondary expression</typeparam>
+        /// <param name="extract1">The primary value expression</param>
+        /// <param name="extract2">The secondary value expression</param>
+        /// <param name="isAscendingOrder">Defines whether comparisons use default (ascending) or reverse (descending) behavior</param>
+        /// <returns><see cref="MultiComparer{T}"/></returns>
+        public static MultiComparer<T> Create<U, V>(
+            Func<T, U> extract1, Func<T, V> extract2, bool isAscendingOrder = true
+        ) where U : IComparable, IComparable<U> 
+          where V : IComparable, IComparable<V>
+        {
+            var compare1 = GenericComparer<T>.Create(extract1);
+            var compare2 = GenericComparer<T>.Create(extract2);
+            return new MultiComparer<T>(compare1, compare2);
+        }
+
+        public int Compare(T x, T y)
+        {
+            int result = 0;
+            int cursor = 0;
+            do
+            {
+                result = _comparers[cursor].Compare(x, y);
+                cursor++;
+            }
+            while (result == 0 && cursor < _comparers.Length);
+
+            return result;
+        }
+
+        int IComparer.Compare(object x, object y)
+        {
+            if ((x is T) == false)
+                throw new ArgumentException(string.Format("x is not an instance of type {0}", typeof(T).Name));
+            if ((y is T) == false)
+                throw new ArgumentException(string.Format("y is not an instance of type {0}", typeof(T).Name));
+
+            return Compare((T) x, (T) y);
+        }
+    }
+}
